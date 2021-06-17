691bf6c791e273a6b86eeb11c4107bd68b2349b5
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2021-06-17 15:40:12 -0300

    Alterações de captura de mês atual.

--- calendario/javascript/calendario.js
+++ calendario/javascript/calendario.js
@@ -29,151 +29,137 @@ var app = {
   }
 }
 app.init();
-
-
-
-
+var data = new Date();
+var dia = data.getDate();
+var mes = data.getMonth();
+var ano = data.getFullYear();
+var meses = [];
+var dias = [];
+var count = 0;
+for (let index = 0; index < 11; index++) {
+  let date = new Date(data.getFullYear(), index, 1); 
+  let stringMes = date.toLocaleString('default', { month: 'long' });
+  meses.push(stringMes.charAt(0).toUpperCase() + stringMes.slice(1));
+}
+for (let index = 6; index <= 12; index++) {
+  let dataAtual = new Date(2009, 11, index); 
+  let stringDia = dataAtual.toLocaleString('default', { weekday: 'long' });
+  dias.push(stringDia.charAt(0).toUpperCase() + stringDia.slice(1));
+}
+function ConcatenarDataAtualComMes() {
+  var diadeHj = dia + " de " + meses[mes];
+  $('#divDia').text(diadeHj);
+  $('#divDiaSemana').text(data.toLocaleString('default', { weekday: 'long' }).toUpperCase());
+}
+function ConcatenarMesAtualComAno(numMes,NumAno) {
+  if (!isNaN(numMes) && numMes >= 0  && !isNaN(NumAno) && NumAno > 0) {
+    let dataAtualcal = document.getElementById('divDataAtual');
+    dataAtualcal.textContent = meses[numMes] + " de " + NumAno.toString();
+  }else{
+    let dataAtualcal = document.getElementById('divDataAtual');
+    dataAtualcal.textContent = meses[data.getMonth()] + " de " + data.getFullYear().toString();
+  }
+}
+function btnAnt_Prox() {
+  ConcatenarMesAtualComAno(mes + count, ano);
+  count++;
+  document.getElementById('btn-left').addEventListener('click', () => lastMonth());
+  document.getElementById('btn-right').addEventListener('click', () => nextMonth());
+  writeMonth(mes);
+  // function EscondeCalendario() {
+  //   var button = document.getElementById("calendario");
+  //   if (button.checked == true) {
+  //     document.getElementById("calendario").hidden = false;
+  //   }
+  //   else {
+  //     document.getElementById("calendario").hidden = false;
+  //   }
+  // }
+}
+function btnAnt_Antir() {
+  ConcatenarMesAtualComAno(mes - count, ano);
+  count--;
+  document.getElementById('btn-left').addEventListener('click', () => lastMonth());
+  document.getElementById('btn-right').addEventListener('click', () => nextMonth());
+  writeMonth(mes);
+  // function EscondeCalendario() {
+  //   var button = document.getElementById("calendario");
+  //   if (button.checked == true) {
+  //     document.getElementById("calendario").hidden = false;
+  //   }
+  //   else {
+  //     document.getElementById("calendario").hidden = false;
+  //   }
+  // }
+}
 $('#left').click(function () {
   configuracoes.container.toggleClass('front');
-  dataAtual();
-  mesAnoAtual();
-  btnAnt_Prox();
+  ConcatenarDataAtualComMes();
+  ConcatenarMesAtualComAno();
+  btnAnt_Antir();
 });
 $('#right').click(function () {
   configuracoes.container.toggleClass('back');
-  dataAtual();
-  mesAnoAtual();
+  ConcatenarDataAtualComMes();
+  ConcatenarMesAtualComAno();
   btnAnt_Prox();
 });
-
-// $( document ).ready(function() {
-
-// });
-
-function dataAtual() {
-  var data = new Date();
-  var diaSemana = data.getDay();
-  var dia = data.getDate();
-  var mes = data.getMonth();
-  var ano = data.getFullYear();
-  var meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
-  var semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado",)
-  var diadeHj = dia + " de " + meses[mes];
-  $('#divDia').text(diadeHj);
-  $('#divDiaSemana').text(semanas[diaSemana].toUpperCase());
-
-
-}
-function mesAnoAtual() {
-  var data = new Date()
-  var mes = data.getMonth();
-  var ano = data.getFullYear();
-  var meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
-  var AnoAtual = ano;
-  var AnoAtual = (meses[mes] + " de " + AnoAtual);
-  $('#divAno').text(AnoAtual);
-}
-var count = 0;
-function btnAnt_Prox() {
-  let nomesMes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-  let dataAtual = new Date();
-  let diaAtual = dataAtual.getDate();
-  let numeroMes = dataAtual.getMonth() + count;
-  let anoAtual = dataAtual.getFullYear();
-  count++;
-
-  let dataAtualcal = document.getElementById('divDataAtual');
-
-  
-  dataAtualcal.textContent = nomesMes[numeroMes] + " de " + anoAtual.toString();
-  
-  document.getElementById('btn-left').addEventListener('click', () => lastMonth());
-  document.getElementById('btn-right').addEventListener('click', () => nextMonth());
-
-
-
-
-  let writeMonth = (month) => {
-    debugger
-    for (var i = startDay(); i > 0 ; i--) {
-      dates.innerHTML += `<span class="last-month">${getTotalDays(month - 1) - (i - 1)}</span>`;
-    }
-
-    for (let i = 1; i <= getTotalDays(month); i++) {
-      if (i === diaAtual) {
-        dates.innerHTML += ` <span class="active">${i}</span>`;
-      } else {
-        dates.innerHTML += ` <span>${i}</span>`;
-      }
-    }
+let writeMonth = function(month) {
+  document.getElementsByClassName('weeks').textContent = '';
+  for (var i = startDay(); i > 0 ; i--) {
+    dates.innerHTML += `<span class="last-month">${getTotalDays(month - 1) - (i - 1)}</span>`;
   }
-
-  let getTotalDays = month => {
-    if (month === -1) month = 11;
-
-    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
-      return 31;
-
-    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
-      return 30;
-
+  for (let i = 1; i <= getTotalDays(month); i++) {
+    if (i === dia) {
+      dates.innerHTML += ` <span class="active">${i}</span>`;
     } else {
-
-      return isLeap() ? 29 : 28;
-    }
-  }
-
-  let isLeap = () => {
-    return ((anoAtual % 100 !== 0) && (anoAtual % 4 === 0) || (anoAtual % 400 === 0));
-  }
-
-  let startDay = () => {
-    debugger
-    let start = new Date(anoAtual, numeroMes, 1);
-    return start.getDay();
-    // return ((start.getDay() - 1) === -1) ? 6 : start.getDay() - 1;
-  }
-
-  let lastMonth = () => {
-
-    if (numeroMes !== 0) {
-      numeroMes--;
-    } else {
-      numeroMes = 11;
-      currentYear--;
-    }
-
-    setNewDate();
-  }
-
-  let nextMonth = () => {
-    if (numeroMes !== 11) {
-      numeroMes++;
-    } else {
-      numeroMes = 0;
-      anoAtual++;
-    }
-
-    setNewDate();
-  }
-
-  let setNewDate = () => {
-    dataAtual.setFullYear(anoAtual, numeroMes, diaAtual);
-    dataAtualcal.textContent = nomesMes[numeroMes] + 'de' + anoAtual.toString();
-    writeMonth(numeroMes);
-  }
-
-  writeMonth(numeroMes);
-
-  window.addEventListener("load", selection());
-  function EscondeCalendario() {
-    var button = document.getElementById("calendario");
-
-    if (button.checked == true) {
-      document.getElementById("calendario").hidden = false;
-    }
-    else {
-      document.getElementById("calendario").hidden = false;
+      dates.innerHTML += ` <span>${i}</span>`;
     }
   }
 }
+let getTotalDays = function(month) {
+  if (month === -1) month = 11;
+  if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+    return 31;
+  } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+    return 30;
+  } else {
+    return isLeap() ? 29 : 28;
+  }
+}
+let isLeap = function()  {
+  return ((ano % 100 !== 0) && (ano % 4 === 0) || (ano % 400 === 0));
+}
+let startDay = function()  {
+  debugger
+  let start = new Date(ano, mes, 1);
+  return start.getDay();
+}
+let lastMonth = function()  {
+  if (mes !== 0) {
+    mes--;
+  } else {
+    mes = 11;
+    ano--;
+  }
+  setNewDate();
+}
+let nextMonth = function()  {
+  if (mes !== 11) {
+    mes++;
+  } else {
+    mes = 0;
+    ano++;
+  }
+  setNewDate();
+}
+let setNewDate = function() {
+  ConcatenarMesAtualComAno(mes,ano);
+  writeMonth(mes);
+}
+
+
+
+
+
+
